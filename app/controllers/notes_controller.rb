6ace664f7da02c0499666239ba666945afead3ce
daichi kondo <kurutoga.kingyo@gmail.com>
2020-11-01 21:56:59 +0900

    日記風アプリ作成中

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,18 +1,22 @@
 class NotesController < ApplicationController
   before_action :create_prev_notes
-  def index
-    @notes = Note.all.order(:date)
-    @today_note = Note.find_by(date: Date.today)
-  end
 
-  def show
-    @note = Note.find(params[:id])
+  def index
+    @date = params[:date].blank? ? Date.today : Date.parse(params[:date])
+
+    @year_range = (@date.year - 1)..(@date.year + 1)
+    @month_range = 1..12
+
+    @notes = Note.where(date: @date.beginning_of_month..@date.end_of_month)
+    @note = if n = Note.find_by(date: @date) then n
+            else Note.create(date: @date)
+            end
   end
 
   def update
     @note = Note.find(params[:id])
     @note.update(note_params)
-    redirect_to notes_path
+    redirect_to notes_path(date: @note.date)
   end
 
   private
@@ -22,7 +26,7 @@ class NotesController < ApplicationController
   end
 
   def create_prev_notes
-    uncreated_dates = [*Date.new(2020, 10, 1)..(Date.today + 7)] - Note.all.pluck(:date)
+    uncreated_dates = [*Date.today.beginning_of_year..Date.today.end_of_year] - Note.all.pluck(:date)
     uncreated_dates.each do |date|
       Note.create(date: date)
     end
