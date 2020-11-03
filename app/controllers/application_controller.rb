7deb9f4eb2a7b406db2dc25782656e86a01cb444
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-11-04 01:13:42 +0900

    シンプル日記 一旦完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    return @current_user_memo if @current_user_memo
+
+    user_id = session[:user_id] || cookies.signed[:user_id]
+    user    = User.find_by(id: user_id)
+    if session[:user_id] || user&.authenticated?(cookies[:remember_token])
+      session[:user_id] ||= user.id
+      @current_user_memo = user
+    end
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,34 +1,43 @@
 class NotesController < ApplicationController
-  before_action :create_prev_notes
+  before_action :check_login
+  before_action :create_empty_notes
 
   def index
-    @date = params[:date].blank? ? Date.today : Date.parse(params[:date])
-
-    @year_range = (@date.year - 1)..(@date.year + 1)
-    @month_range = 1..12
-
-    @notes = Note.where(date: @date.beginning_of_month..@date.end_of_month)
-    @note = if n = Note.find_by(date: @date) then n
-            else Note.create(date: @date)
-            end
+    @current_user = current_user
+    # 日付をパラメータで指定   => 指定日とその月を、編集状態で表示
+    # 日付をパラメータで未指定 => 今日と今月を、通常状態で表示
+    @date  = (params[:date].blank? || params[:date] == 'today') ? Date.today : Date.parse(params[:date])
+    @edit  = params[:edit]
+    @notes = @current_user.notes.where(date: @date.beginning_of_month..@date.end_of_month)
+    @note  = @current_user.notes.find_by(date: @date)
   end
 
   def update
-    @note = Note.find(params[:id])
-    @note.update(note_params)
-    redirect_to notes_path(date: @note.date)
+    note = current_user.notes.find(params[:id])
+    if note.update(note_params)
+      flash[:edit_success] = 'ok'
+    end
+    redirect_to notes_path(date: note.date, edit: true)
   end
 
   private
+
+  def check_login
+    if current_user.nil?
+      redirect_to root_path
+    end
+  end
 
   def note_params
     params.require(:note).permit(:text, :date)
   end
 
-  def create_prev_notes
-    uncreated_dates = [*Date.today.beginning_of_year..Date.today.end_of_year] - Note.all.pluck(:date)
-    uncreated_dates.each do |date|
-      Note.create(date: date)
-    end
+  def create_empty_notes
+    has_note_dates = current_user.notes.map(&:date)
+    target_dates   = (Date.today.beginning_of_year..Date.today.next_year.end_of_year).to_a
+    empty_notes = (target_dates - has_note_dates).map { |target_date|
+      {user_id: current_user.id, date: target_date}
+    }
+    Note.import empty_notes
   end
 end
