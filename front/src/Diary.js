3869ce2a7b3dbbcecb0562b6101477b68b0df2bb
kondodaichi <kondodaichi@kondodaichinoMacBook-ea.local>
2021-01-30 21:58:35 +0900

    use-state-with-callbackを入れて、タスクツリーをDBのレコード風に変換するのができた

--- front/src/Diary.js
+++ front/src/Diary.js
@@ -1,29 +1,31 @@
-import React, { Component, useState, useContext, createContext} from 'react';
+import React, { Component, useState, useContext, createContext, useRef, createRef} from 'react';
 import SortableTree, { addNodeUnderParent, removeNode } from 'react-sortable-tree';
 import 'react-sortable-tree/style.css';
 import FileExplorerTheme from 'react-sortable-tree-theme-minimal';
+import useStateWithCallback from 'use-state-with-callback';
 
 const MyContext = createContext(() => {})
 
 const Diary = props => {
   const today = new Date()
-  const [edit, setEdit] = useState(false)
+  const [edit, setEdit] = useStateWithCallback(false, () => {
+    console.log(nodes.map((n, i) => [n.ref.current, n.parentKey, i]))
+  })
   const [treeData, setTreeData] = useState([])
-  const changeEdit = e => {
-    setEdit(prev => !prev)
-    if(edit){
-      console.log(treeData)
-    }
-  }
+  const [nodes, setNodes] = useState([])
   const addChild = rowInfo => {
+    const parentKey = rowInfo ? rowInfo.treeIndex : undefined
+    const ref = createRef(null)
+    const node = { ref: ref, parentKey: parentKey }
+    setNodes(prevNodes => [...prevNodes, node])
     const newNode = {
-      title: <TaskInput/>,
+      title: <TaskInput ref={ref}/>,
       expanded: true
     }
     setTreeData(addNodeUnderParent({
       treeData: treeData,
       newNode: newNode,
-      parentKey: rowInfo ? rowInfo.treeIndex : undefined,
+      parentKey: parentKey,
       getNodeKey: ({ treeIndex }) => treeIndex
     }).treeData)
   }
@@ -37,7 +39,7 @@ const Diary = props => {
   return (
     <div>
       <h2>{today.getDate()}日</h2>
-      <button onClick={changeEdit}>{edit ? 'save' : 'edit'}</button>
+      <button onClick={e => setEdit(e => !e)}>{edit ? 'save' : 'edit'}</button>
       {edit && <button onClick={addChild}>+</button>}
       <div style={{ height: 800 }}>
         <MyContext.Provider value={edit}
@@ -58,13 +60,17 @@ const Diary = props => {
   )
 }
 
-const TaskInput = props => {
+const TaskInput = React.forwardRef((props, ref) => {
   const [name, setName] = useState('new')
   const edit = useContext(MyContext)
   return (
-    edit ? <input type="text" value={name} onChange={e => setName(e.target.value)}></input> : name
+    <p ref={ref}>{
+      edit
+      ? <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
+      : name
+    }</p>
   )
-}
+})
 
 
 export default Diary;
