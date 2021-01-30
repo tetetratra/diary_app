96b8b37994357eee696f3828bdfdd2432d4dacbd
kondodaichi <kondodaichi@kondodaichinoMacBook-ea.local>
2021-01-30 17:20:47 +0900

    react sortable treeを正しく追加できた

--- front/src/Diary.js
+++ front/src/Diary.js
@@ -1,76 +1,79 @@
-import React, {useState, useContext, createContext} from 'react';
-import SortableTree from 'react-sortable-tree';
-
-const MyContext = createContext(() => {})
+import React, { Component, useState, useContext, createContext} from 'react';
+import SortableTree, { addNodeUnderParent, removeNode } from 'react-sortable-tree';
+import 'react-sortable-tree/style.css';
+import FileExplorerTheme from 'react-sortable-tree-theme-minimal';
 
 const Diary = props => {
   const today = new Date()
   const [edit, setEdit] = useState(false)
+  const [treeData, setTreeData] = useState([
+    {
+      title: 'Chicken',
+      children: [
+        {
+          title: 'Egg'
+        }
+      ]
+    },
+    { title: 'Fish',
+      children: [
+        {
+          title: 'fingerline'
+        }
+      ]
+    },
+  ])
   const changeEdit = e => {
     setEdit(prev => !prev)
     if(edit){
-      alert('saved!')
+      console.log(treeData)
     }
+  }
+  const addChild = rowInfo => {
+    const newNode = {
+      title: 'new',
+      expanded: true
+    }
+    const newTree = addNodeUnderParent({
+      treeData: treeData,
+      newNode: newNode,
+      parentKey: rowInfo ? rowInfo.treeIndex : undefined,
+      getNodeKey: ({ treeIndex }) => treeIndex
+    })
+    setTreeData(newTree.treeData)
+  }
+  const deleteSelf = rowInfo => {
+    const newTree = removeNode({
+      treeData: treeData,
+      path: rowInfo.path,
+      getNodeKey: ({ treeIndex }) => treeIndex
+    })
+    setTreeData(newTree.treeData)
+  }
+  const initTree = () => {
+    setTreeData([
+      { title: 'root', expanded: true },
+    ])
   }
   return (
     <div>
       <h2>{today.getDate()}日</h2>
-      <MyContext.Provider value={edit}>
-        <button onClick={changeEdit}>{edit ? 'save' : 'edit'}</button>
-        <Task isRoot key={0}/>
-        <Note />
-      </MyContext.Provider>
+      <button onClick={changeEdit}>{edit ? 'save' : 'edit'}</button>
+      {treeData.length === 0 && <button onClick={initTree}>init task tree</button>}
+      <div style={{ height: 800 }}>
+        <SortableTree
+          treeData={treeData}
+          onChange={treeData => setTreeData(treeData)}
+          theme={FileExplorerTheme}
+          generateNodeProps={rowInfo => ({
+            buttons: [
+              <button onClick={e => addChild(rowInfo)}>+</button>,
+              <button onClick={e => deleteSelf(rowInfo)}>-</button>
+            ]
+          })}
+        />
+      </div>
     </div>
-  )
-}
-
-const Task = ({isRoot}) =>{
-  const [deleted, setDeleted] = useState(false)
-  const [text, setText] = useState(isRoot ? 'TODO' : '')
-  const [children, setChildren] = useState([])
-  const [status, setStatus] = useState(0)
-  const addTask = () => {
-    setChildren(prevChildren => [...prevChildren, <Task key={prevChildren.length}/>])
-  }
-  const deleteTask = () => {
-    setDeleted(true)
-  }
-  const changeText = e => {
-    setText(e.target.value)
-  }
-  const changeStatus = e => {
-    setStatus(prevStatus => (prevStatus + 1) % 3)
-  }
-  const edit = useContext(MyContext)
-  return (!deleted &&
-    <ul>
-      {
-        <li>{
-          edit
-          ? <>
-            <button onClick={addTask}>+</button>
-            {!isRoot && <button onClick={deleteTask}>-</button>}
-            <button onClick={changeStatus}>{status}</button>
-            <input type='text' value={text} onChange={changeText}></input>
-          </>
-          : `[${status}] ${text}`
-        }</li>
-      }
-      {children}
-    </ul>
-  )
-}
-
-const Note = props => {
-  const [text, setText] = useState('')
-  const changeText = e => {
-    setText(e.target.value)
-  }
-  const edit = useContext(MyContext)
-  return (
-    edit
-    ? <textarea value={text} onChange={changeText}></textarea>
-    : <p>{text}</p>
   )
 }
 
