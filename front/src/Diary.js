1fe7478b6ed152c3217ae77d22befdb39fcb4267
kondodaichi <kondodaichi@kondodaichinoMacBook-ea.local>
2021-01-31 00:43:15 +0900

    material-uiを入れて、タスクのチェックボックスを作った

--- front/src/Diary.js
+++ front/src/Diary.js
@@ -3,6 +3,8 @@ import SortableTree, { addNodeUnderParent, removeNode } from 'react-sortable-tre
 import 'react-sortable-tree/style.css';
 import FileExplorerTheme from 'react-sortable-tree-theme-minimal';
 import useStateWithCallback from 'use-state-with-callback';
+import { TextField, Checkbox } from '@material-ui/core';
+import './Diary.css';
 
 const MyContext = createContext(() => {})
 
@@ -16,20 +18,22 @@ const Diary = props => {
   const addChild = rowInfo => {
     const parentKey = rowInfo ? rowInfo.treeIndex : undefined
     const ref = createRef(null)
-    const node = { ref: ref, parentKey: parentKey }
-    setNodes(prevNodes => [...prevNodes, node])
     const newNode = {
       title: <TaskInput ref={ref}/>,
       expanded: true
     }
-    setTreeData(addNodeUnderParent({
+    const newTree = addNodeUnderParent({
       treeData: treeData,
       newNode: newNode,
       parentKey: parentKey,
       getNodeKey: ({ treeIndex }) => treeIndex
-    }).treeData)
+    })
+    setTreeData(newTree.treeData)
+    const node = { ref: ref, parentKey: parentKey, treeIndex: newTree.treeIndex }
+    setNodes(prevNodes => [...prevNodes, node])
   }
   const deleteSelf = rowInfo => {
+    setNodes(prevNodes => prevNodes.filter(n => n.treeIndex === rowInfo.treeIndex))
     setTreeData(removeNode({
       treeData: treeData,
       path: rowInfo.path,
@@ -41,7 +45,7 @@ const Diary = props => {
       <h2>{today.getDate()}日</h2>
       <button onClick={e => setEdit(e => !e)}>{edit ? 'save' : 'edit'}</button>
       {edit && <button onClick={addChild}>+</button>}
-      <div style={{ height: 800 }}>
+      <div className={'task-tree'} style={{ height: (nodes.length === 0 ? 62 : nodes.length * 62) + 50 }}>
         <MyContext.Provider value={edit}
           children={<SortableTree
             treeData={treeData}
@@ -53,24 +57,46 @@ const Diary = props => {
                 (edit && <button onClick={e => deleteSelf(rowInfo)}>-</button>)
               ]
             })}
+            canDrag={edit}
           />}
         />
       </div>
+      <Note/>
     </div>
   )
 }
 
+const statusToCheckBox = status => (
+  {
+    0: <span>・</span>,
+    1: <input type='checkbox'></input>,
+    2: <input type='checkbox' checked></input>
+  }[status]
+)
+
 const TaskInput = React.forwardRef((props, ref) => {
   const [name, setName] = useState('new')
+  const [status, setStatus] = useState(0)
   const edit = useContext(MyContext)
   return (
-    <p ref={ref}>{
-      edit
-      ? <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
-      : name
-    }</p>
+    <span ref={ref}>
+      <span onClick={() => setStatus(s => (s + 1) % 3)}>{statusToCheckBox(status)}</span>
+      {edit ? <input type="text" value={name} onChange={e => setName(e.target.value)}></input> : name}
+    </span>
   )
 })
 
+const Note = props => {
+  return (
+    <TextField
+      id="outlined-multiline-static"
+      label="Multiline"
+      multiline
+      rows={6}
+      defaultValue="Default Value"
+      variant="outlined"
+    />
+  )
+}
 
 export default Diary;
