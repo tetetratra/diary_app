3e126cb7a932e8c879c8872d6962c60625121c7b
kondodaichi <kondodaichi@kondodaichinoMacBook-ea.local>
2021-01-31 14:22:26 +0900

    react routerを導入

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import Diary from './Diary.js';
 import Calendar from './Calendar.js';
 
-const App = props => (
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Home = props => (
   <div>
     <h1>md-iary</h1>
     <Calendar />
     <Diary />
   </div>
+)
+
+const App = props => (
+  <Router>
+    <Route exact path="/" component={Home}></Route>
+  </Router>
 )
 
 export default App;
